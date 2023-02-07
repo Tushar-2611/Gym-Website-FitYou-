322e0746c43d6f738a067c6cefa862bed5d55370
Tushar Kadam <109825794+Tushar-2611@users.noreply.github.com>
2023-02-07 19:46:08 +0530

    Add files via upload

---
+++ PHP/Structure/footer.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Stick+No+Bills:wght@600&display=swap" rel="stylesheet">
+
+  
+    <link rel="stylesheet" href="home.css">
+    <title>About | FIT YOU</title>
+</head>
+<body>
+  <footer class="section-footer" id="footer">
+      <div class="footer-box container">
+        <div class="contact-details">
+          <h2 class="margin-bottom"><span class="yellow">FIT</span>YOU</h2>
+
+          <ul class="contact-data">
+            <li class="location">Carter Rd, Bandra West, Mumbai</li>
+            <li class="phone">+00 000 000 00</li>
+            <li class="email">fityou@gmail.com</li>
+            <li class="social">
+              <img
+                src="img/logo-whatsapp.svg"
+                alt="whatsapp icon"
+                width="35"
+                height="35"
+              />
+
+              <img
+                src="img/logo-facebook.svg"
+                alt="facebook icon"
+                width="35"
+                height="35"
+              />
+
+              <img
+                src="img/logo-instagram.svg"
+                alt="instagram icon"
+                width="35"
+                height="35"
+              />
+
+              <img
+                src="img/logo-twitter.svg"
+                alt="twitter icon"
+                width="35"
+                height="35"
+              />
+            </li>
+          </ul>
+        </div>
+
+        <nav class="footer-nav" aria-label="navigation">
+          <div class="nav-name">Quick Links</div>
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="course.php">Courses</a></li>
+            <li><a href="trainer.php">Trainers</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="faq.php">FAQ</a></li>
+
+
+            
+          </ul>
+        </nav>
+
+
+
+
+        <div class="login-logout">
+
+          <img src="img/sitelogo.jpg" alt="Logo" class="sitelogo">
+
+          <ul>
+            <li>
+              <button id="register_btnf" >
+              <a href="login/register.php">Register</a></button>
+            </li>
+
+            <li>
+              <button id="logout_btnf" >
+              <a href="login/logout.php">Log Out</a></button>
+            </li>
+          </ul>
+
+
+        </div>
+
+        <div class="legel">
+          <p class="text">Copyright &copy; by FIT YOU&nbsp;</p>
+          <a href="Google.com">
+          <span class="yelllow">FIT</span>YOU</p>
+
+          </a>
+        </div>
+      </div>
+
+    </footer>
+
+    <script src="index.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+</body>
+</html>
